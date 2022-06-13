163e851515c5336c63426c0e45f80d3dc63576b2
haimnewclass <haim@newclass.co.il>
2022-06-13 15:01:49 +0300

    Add project files.

---
+++ CS-Gym/Meet1-Tuple.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Gym
+{
+    internal class Meet1_Tuple
+    {
+       public void Run()
+       {
+
+       }
+    }
+}
